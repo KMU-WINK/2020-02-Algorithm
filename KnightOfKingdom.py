1ae962899c41b4fe21b49d8e16100ec76a01a8cf
sowish23 <soso2266@naver.com>
2020-11-11 03:59:38 +0900

    왕실의나이트 함수화

--- KnightOfKingdom.py
+++ KnightOfKingdom.py
@@ -6,21 +6,19 @@ location[1] = int(location[1])
 
 cnt = 0
 
+def setCnt(idx) :
+    global cnt
+    cnt += 2
+    if location[idx] == 1 or location[idx] == 8 :
+        cnt -= 1
+    
 if location[0] <= 6 :
-    cnt += 2
-    if location[1] == 1 or location[1] == 8 :
-        cnt -= 1
+    setCnt(1)
 if location[0] >= 3 :
-    cnt += 2
-    if location[1] == 1 or location[1] == 8 :
-        cnt -= 1
+    setCnt(1)
 if location[1] <= 6 :
-    cnt += 2
-    if location[0] == 1 or location[0] == 8 :
-        cnt -= 1
+    setCnt(0)
 if location[1] >= 3 :
-    cnt += 2
-    if location[0] == 1 or location[0] == 8 :
-        cnt -= 1
+    setCnt(0)
 
 print(cnt)

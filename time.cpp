838e889ad36b497b3ef40aa36894bf9f50fee822
Lsyeon <baekgu1011@naver.com>
2020-11-12 13:29:00 +0900

    시각

---
+++ time.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int result = 0;
+    cin >> n;
+    int arr[] = {10, 6, 10, 6, n%10+1, n/10+1}; //초, 분, 시간 +1은 *했을때 0이 나오면 x
+    for (int i = 0; i < 5; i++)
+    {
+        result += arr[5]*arr[4]*arr[3]*arr[2]*arr[1]*arr[0]/arr[i];
+        arr[i] -= 1;
+    }
+    cout << result<< endl; 
+}

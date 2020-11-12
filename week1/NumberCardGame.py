0d837097119f25b509debe36e192d839fd87f4b1
bell2lee <paperlee@kookmin.ac.kr>
2020-11-12 15:01:55 +0900

    인생

--- week1/NumberCardGame.py
+++ week1/NumberCardGame.py
@@ -1,6 +1,4 @@
 import sys
 
 n, m = tuple(map(int, sys.stdin.readline().split()))
-matrix = [min(list(map(int, sys.stdin.readline().split()))) for i in range(n)]
-
-print(max(matrix))
+print(max([min(list(map(int, sys.stdin.readline().split()))) for i in range(n)]))

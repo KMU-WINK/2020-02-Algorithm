49b24b304c5c26d2b2392352b2fe4970e34d1311
Lsyeon <baekgu1011@naver.com>
2020-11-12 18:33:21 +0900

    왕실의 나이트

--- royalNight.cpp
+++ royalNight.cpp
@@ -1,1 +1,21 @@
-dddfsdfas
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+    int count = 0;
+    int row[] = {2, 2, -2, -2, 1, 1, -1, -1};
+    int col[] = {1, -1, 1, -1, 2, -2, 2, -2};
+    for (int i = 0; i < 8; i++)
+    {
+        int input[] = {a[0]-96, a[1]-48}; //아스키코드 a1 -> {1, 1}
+        input[0]+=row[i];
+        input[1]+=col[i];
+        if(input[0]> 0 && input[1]>0){
+            count += 1;
+        }
+    }
+    cout << count << endl;
+}

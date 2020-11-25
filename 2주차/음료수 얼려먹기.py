73f77e78f4613962510bedb865801057624ae75f
taehoonkim <rlaxogns4504@gmail.com>
2020-11-26 01:20:08 +0900

    음료수 얼려 먹기

---
+++ 2주차/음료수 얼려먹기.py
@@ -0,0 +1,30 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+def dfs(v1, v2):
+    if v1 < 0 or v1 >= n or v2 < 0 or v2 >= m:
+        return False
+
+    if not graph[v1][v2]:  # 구멍이 뚫려있다면
+        graph[v1][v2] = 1
+        dfs(v1 + 1, v2)
+        dfs(v1, v2 + 1)
+        dfs(v1 - 1, v2)
+        dfs(v1, v2 - 1)
+        return True
+    return False
+
+
+count = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            count += 1
+
+print(count)

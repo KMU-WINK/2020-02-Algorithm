a294d12d1f2e3e7d0d4df10afc5f8ed5848a4c4f
hyeonsang010716 <67507183+hyeonsang010716@users.noreply.github.com>
2020-11-30 00:12:03 +0900

    Update 성적이 낮은 순서로 학생 출력하기.py

--- 성적이 낮은 순서로 학생 출력하기.py
+++ 성적이 낮은 순서로 학생 출력하기.py
@@ -4,7 +4,7 @@ for i in range(N):
     name, grade = input().split()
     p_list[name] = grade
 
-sorted(p_list.keys())
+p_list = dict(sorted(p_list.items(),key=lambda x:x[1]))
 for key, value in p_list.items():
     print(key, end=' ')
 

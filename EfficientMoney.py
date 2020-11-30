70d8e394634ba01ed7128079051d5aa562df5cd9
sowish23 <soso2266@naver.com>
2020-12-01 03:22:18 +0900

    효율적인 화폐구성

---
+++ EfficientMoney.py
@@ -0,0 +1,20 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+array = []
+for i in range(n):
+    array.append(int(input()))
+
+d = [10001] * (m + 1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(array[i], m + 1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j - array[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])

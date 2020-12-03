158762d9ed4821f7442922dd85a2d9bfc399d70b
20171609 <clw3wb@naver.com>
2020-12-03 22:18:58 +0900

    효율적인 화폐 구성 수정

--- week3/money.py
+++ week3/money.py
@@ -5,38 +5,24 @@ N, M = tuple(map(int, sys.stdin.readline().split()))
 money = []
 
 for i in range(N):
-    money.append(int(sys.stdin.readline()))
+    don = int(sys.stdin.readline())
+    if(don <= M):
+        money.append(don)
 
-money.sort(reverse=True)
+check = [-1 for x in range(M+1)]
 
-result = []
+for i in money:
+    check[i] = 1
 
-for i in range(len(money)):
-    count = 0
-    total = M
-    check = i
+for i in range(len(check)):
+    for j in money:
+        if(check[i] == -1 or i+j > M):
+            continue
 
-    while(total != 0):
-        if(total >= money[check]):
-            total -= money[check]
-            count += 1
+        if(check[i+j] == -1):
+            check[i+j] = check[i] + 1
 
-        else:
-            check += 1
+        if(check[i]+1 < check[i+j]):
+            check[i+j] = check[i] + 1
 
-        if(check == len(money)):
-            result.append(-1)
-            break
-
-    if(total == 0):
-        result.append(count)
-
-small = M # M은 나올수 있는 최댓값.
-check2 = 0 # -1로 채워져 있는지 확인
-
-for i in range(len(result)):
-    if(result[i] != -1 and small > result[i]):
-        check2 = 1
-        small = result[i]
-
-print(small if check2 else -1)
+print(check[-1])

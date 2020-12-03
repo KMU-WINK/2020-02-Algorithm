26d8430b6c9efbafa7baa35860087997568b1dc5
Shin-ba <72019064+Shin-723@users.noreply.github.com>
2020-12-03 20:03:46 +0900

    Update Effective_money.py

--- Grade_output.out.dSYM/Contents/Effective_money.py
+++ Grade_output.out.dSYM/Contents/Effective_money.py
@@ -1,9 +1,1 @@
-n, m = map(int, input().split())
-val = [int(input()) for _ in range(n)]
-print(val)
-d = [10001]*10001
-d[0] = 0
-for v in val:
-    for j in range(v, m+1):
-        d[j] = min(d[j], d[j-v]+1)
-print(d[m])
+

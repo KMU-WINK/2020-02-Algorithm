47b43901b8390a641a29c5f6f9195dec65963a5e
Lsyeon <baekgu1011@naver.com>
2020-12-03 22:38:18 +0900

    숫자카드게임 수정

--- cardGame.cpp
+++ cardGame.cpp
@@ -6,15 +6,15 @@ int main(){
     int n, m, a, b;
     int x = 0;
     cin >> n >> m;
-    for(int i =0; i<m; i++){
-        for(int j=0; j<n; j++){
+    for(int i =0; i<n; i++){
+        for(int j=0; j<m; j++){
             cin >> b;
             if(j == 0){
-                a = b;
+                a = b; // a의 초기값 부여
             }
-            a = min(a, b);
+            a = min(a, b); // 작은거 찾고
         }
-        x = max(x, a);
+        x = max(x, a); // 그 중 큰거
     }
     cout << x << endl;
 }

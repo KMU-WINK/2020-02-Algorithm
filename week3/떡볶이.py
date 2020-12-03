fe0d6a31ae24a3fbe05b4e3e93943f8e8a9bdd24
bell2lee <paperlee@kookmin.ac.kr>
2020-12-03 19:46:24 +0900

    떡볶이 문제

---
+++ week3/떡볶이.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+riceCake = list(map(int, input().split()))
+
+cutterHeight = {
+    'min': 0,
+    'max': max(riceCake),
+}
+
+while True:
+    settingHeight = cutterHeight['min'] + ((cutterHeight['max'] - cutterHeight['min']) // 2) # 비교 높이 설정
+    awserLength = sum(map(lambda x: x - settingHeight if x - settingHeight > 0 else 0, riceCake)) # 잘린 떡 합 구하기
+    print(settingHeight, awserLength)
+
+    if awserLength < m:
+        cutterHeight['max'] = settingHeight - 1
+    elif awserLength > m:
+        cutterHeight['min'] = settingHeight + 1
+    else:
+        print(settingHeight)
+        break

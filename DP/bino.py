f71edf3048d3267251ecd09eefdccd1ca0a7fcac
taehoonkim <rlaxogns4504@gmail.com>
2020-12-31 21:31:24 +0900

    이항계수

--- DP/bino.py
+++ DP/bino.py
@@ -1,21 +1,16 @@
-def bino_coef(n, r):
-    if r == 0 or r == n:
-        return 1
-    return bino_coef(n - 1, r) + bino_coef(n - 1, r - 1)
-
-def bino_coef_cache(n, r):
-    cache = [[0 for _ in range(r + 1)] for _ in range(n + 1)]
-
-    for i in range(n + 1):
-        cache[i][0] = 1
-    for i in range(r + 1):
-        cache[i][i] = 1
-
+def bin(n, r):
+    if (r > n // 2):
+        r = n - r
+    b = [0] * (r + 1)
+    b[0] = 1
     for i in range(1, n + 1):
-        for j in range(1, r + 1):
-            cache[i][j] = cache[i - 1][j] + cache[i - 1][j - 1]
+        j = min(i, r)
+        while (j > 0):
+            b[j] = b[j] + b[j - 1]
+            j -= 1
+        for n in range(min(i, r) + 1):
+            print(b[n], end = " ")
+        print()
+    return b[r]
 
-    return cache[n][r]
-
-print(bino_coef(4, 2))
-print(bino_coef_cache(4, 2))
+print(bin(9, 3))

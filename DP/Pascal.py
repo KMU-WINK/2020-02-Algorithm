5eb6770878990e5e85c6a5389ed66b76703dbd07
H0zzae <39540561+H0zzae@users.noreply.github.com>
2020-12-31 21:06:05 +0900

    -print

--- DP/Pascal.py
+++ DP/Pascal.py
@@ -7,7 +7,6 @@ for i in range(X+1):
         num[i]=1
     else:
         num[i] = num[i-1]+i+1
-print(num)
 def pascal(x):
     for i in range(X+1):
         if i==0:
@@ -20,7 +19,6 @@ def pascal(x):
                     dp[pren] = 1
                 elif j==n-1:
                     dp[num[i]-1]=1
-                    print(n-1,dp[n])
                 else:
                     dp[pren+j] = dp[pren-n+j]+dp[pren-n+j+1]
     return dp[:dp.index(0)]

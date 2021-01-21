587536a59340145d24ab43e6f27811238ed69386
이소정 <66251759+i-soj-ng@users.noreply.github.com>
2021-01-21 20:08:55 +0900

    Add files via upload

---
+++ 백준11404번(플로이드).py
@@ -0,0 +1,26 @@
+n = int(input())
+m = int(input())
+inf = 100000000
+
+graph = [[inf] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if graph[i][j] >= inf:
+            graph[i][j] = 0
+        print(graph[i][j], end=' ')
+    print()
